fc0a7483fbcd6407df2e3292c5b2ffeda8678945
aijujes <108979766+aijujes@users.noreply.github.com>
2022-09-05 13:04:05 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php  
+		$servername = "localhost";  
+		$username = "root";  
+		$password = "aju";  
+		$conn = mysqli_connect($servername , $username , $password,"test") or die("unable to connect to host"); 
+?>   
